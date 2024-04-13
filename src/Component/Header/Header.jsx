30ab05910d15f70e4d2da29ec9d96c592bbbd69b
saifulislam735 <mohimshihab735@gmail.com>
2024-04-14 01:56:09 +0600

    header done

--- src/Component/Header/Header.jsx
+++ src/Component/Header/Header.jsx
@@ -1,9 +1,20 @@
-
+import './Header.css'
+import logo from '../../assets/logo.jpg'
 const Header = () => {
     return (
-        <div>
-            <p>This is player bidding site</p>
-        </div>
+        <nav className="headerStyle">
+            <div>
+                <img className='logo' src={logo} alt="" />
+            </div>
+            <div>
+                <ul className='menu'>
+                    <li>Home</li>
+                    <li>List</li>
+                    <li>About Us</li>
+                    <li>Contact</li>
+                </ul>
+            </div>
+        </nav>
     );
 };
 
